bdf44ea1233dc4d91ed71a223d4797e8d2c4a59b
Abdu <abdurahmann.benissa@ucalgary.ca>
2026-05-13 09:45:18 -0400

    added random number, character generation, word and key generation functions

--- Toolex/clsUtil.h
+++ Toolex/clsUtil.h
@@ -6,6 +6,109 @@ using namespace std;
 
 class clsUtil
 {
+public:
+    enum enCharType {
+        SamallLetter = 1, CapitalLetter = 2,
+        Digit = 3, MixChars = 4, SpecialCharacter = 5
+    };
+
+    static void  Srand()
+    {
+        //Seeds the random number generator in C++, called only once
+        srand((unsigned)time(NULL));
+    }
+
+    static  int RandomNumber(int From, int To)
+    {
+        //Function to generate a random number
+        int randNum = rand() % (To - From + 1) + From;
+        return randNum;
+    }
+
+    static char GetRandomCharacter(enCharType CharType)
+    {
+
+        //updated this method to accept mixchars
+        if (CharType == MixChars)
+        {
+            //Capital/Samll/Digits only
+            CharType = (enCharType)RandomNumber(1, 3);
+
+        }
+
+        switch (CharType)
+        {
+
+        case enCharType::SamallLetter:
+        {
+            return char(RandomNumber(97, 122));
+            break;
+        }
+        case enCharType::CapitalLetter:
+        {
+            return char(RandomNumber(65, 90));
+            break;
+        }
+        case enCharType::SpecialCharacter:
+        {
+            return char(RandomNumber(33, 47));
+            break;
+        }
+        case enCharType::Digit:
+        {
+            return char(RandomNumber(48, 57));
+            break;
+        }
+    defualt:
+        {
+            return char(RandomNumber(65, 90));
+            break;
+        }
+        }
+    }
+
+    static  string GenerateWord(enCharType CharType, short Length)
+
+    {
+        string Word;
+
+        for (int i = 1; i <= Length; i++)
+
+        {
+
+            Word = Word + GetRandomCharacter(CharType);
+
+        }
+        return Word;
+    }
+
+    static string  GenerateKey(enCharType CharType = CapitalLetter)
+    {
+
+        string Key = "";
+
+
+        Key = GenerateWord(CharType, 4) + "-";
+        Key = Key + GenerateWord(CharType, 4) + "-";
+        Key = Key + GenerateWord(CharType, 4) + "-";
+        Key = Key + GenerateWord(CharType, 4);
+
+
+        return Key;
+    }
+
+    static void GenerateKeys(short NumberOfKeys, enCharType CharType)
+    {
+
+        for (int i = 1; i <= NumberOfKeys; i++)
+
+        {
+            cout << "Key [" << i << "] : ";
+            cout << GenerateKey(CharType) << endl;
+        }
+
+    }
+
 
 };
 
